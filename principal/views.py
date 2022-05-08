8b60aefd09d6a23c8fe8250b98394f22f415e8c2
Matheus Sanches <matheussanchesleme@gmail.com>
2022-05-08 19:48:06 -0300

    Mudança no template

--- principal/views.py
+++ principal/views.py
@@ -3,22 +3,22 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    return render(request=request,template_name="principal/index.html")
+    return render(request=request,template_name="principal/index.html", context={"nome":"Início"})
 
 def calculadora(request):
-    return render(request=request,template_name="principal/calculadora.html")
+    return render(request=request,template_name="principal/calculadora.html", context={"nome":"Calculadora"})
 
 def aprendaeconomizar(request):
-    return render(request=request,template_name="principal/aprendaeconomizar.html")
+    return render(request=request,template_name="principal/aprendaeconomizar.html", context={"nome":"Aprenda a Economizar"})
 
 def resultados(request):
-    return render(request=request,template_name="principal/resultados.html")
+    return render(request=request,template_name="principal/resultados.html", context={"nome":"Resultados"})
 
 def referencias(request):
-    return render(request=request,template_name="principal/referencias.html")
+    return render(request=request,template_name="principal/referencias.html", context={"nome":"Referências"})
 
 def sobre(request):
-    return render(request=request,template_name="principal/sobre.html")
+    return render(request=request,template_name="principal/sobre.html", context={"nome":"Sobre"})
 
 def pagina_inexistente(request):
-    return render(request=request, template_name="principal/404.html")
+    return render(request=request, template_name="principal/404.html", context={"nome":"404"})
