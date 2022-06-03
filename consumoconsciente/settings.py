af10ccb11ac31cfa43bc3dc891befc20e7393492
Valdeci Santos de Miranda <66446342+ValdeciMiranda@users.noreply.github.com>
2022-06-03 11:14:31 -0300

    Adição do ajuste de tamanho de fonte
    Falta ainda adicionar o auto contraste, está em andamento.

--- consumoconsciente/settings.py
+++ consumoconsciente/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY') Atenção! o import do config aqui pode trazer o erro "decouple.UndefinedValueError: SECRET_KEY not found. Declare it as envvar or define a default value."  
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
